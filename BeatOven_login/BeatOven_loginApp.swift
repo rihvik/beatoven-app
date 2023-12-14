ee9f2a6277c60bca1d3769d18169d342606cf389
mkswagger <mathangykrishna16@gmail.com>
2023-12-14 16:54:47 +0530

    login page initiated

---
+++ BeatOven_login/BeatOven_loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatOven_loginApp.swift
+//  BeatOven_login
+//
+//  Created by mathangy on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BeatOven_loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeatOven_login/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeatOven_login
+//
+//  Created by mathangy on 13/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
