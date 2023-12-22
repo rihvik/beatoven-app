3dfe64a82b5ab2a600bda35dfeda2d9cdf4ad421
aahlaad26 <aahlaad26092002@gmail.com>
2023-12-22 21:33:30 +0530

    TabBar Created

--- TabBarControllerBeatOven/ContentView.swift
+++ TabBarControllerBeatOven/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            VStack{
+            VStack{ 
                 TabView(selection: $selectedTab){
                     ForEach(Tab.allCases, id: \.rawValue){ tab in
                         HStack{
