03db21e71c35ff57efd4ba8080818c573886672b
mkswagger <mathangykrishna16@gmail.com>
2023-12-25 21:28:25 +0530

    fixed the img to top in signin page and got the rectangle set

--- BeatOven_login/Views/SignupView.swift
+++ BeatOven_login/Views/SignupView.swift
@@ -4,14 +4,32 @@
 //
 //  Created by mathangy on 14/12/23.
 //
-
 import SwiftUI
 
 struct SignUpView: View {
     var body: some View {
-        Text("Welcome to SignUp Page!")
-            .font(.title)
-            .padding()
+        ZStack {
+            VStack(spacing: 0) {
+                            Image("signup-img") // Replace "band" with your image name
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.65)
+                                .edgesIgnoringSafeArea(.top)
+                                .frame(width: 140, height: 140)
+                                .offset(y: -180)// Pin the image to the top
+
+                VStack {
+                    // SignUpView content
+                }
+                .padding(.top, 5) // Adjust the top padding as needed
+            }
+
+            Rectangle()
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.6)
+                .foregroundColor(Color(red: 0.988, green: 0.87, blue: 0.82)) // Set the background color to match the image background
+                .offset(y: UIScreen.main.bounds.height * 0.35) // Adjust the offset to place the rectangle at the bottom
+        }
+        .navigationBarHidden(true)
     }
 }
 
