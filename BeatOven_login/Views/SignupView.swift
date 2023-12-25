b8d18ad71c364ba7ea0e21785596f9678da5b9e4
mkswagger <mathangykrishna16@gmail.com>
2023-12-25 21:56:59 +0530

    filter image for the signup , buttons placed

--- BeatOven_login/Views/SignupView.swift
+++ BeatOven_login/Views/SignupView.swift
@@ -10,13 +10,13 @@ struct SignUpView: View {
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
-                            Image("signup-img") // Replace "band" with your image name
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.65)
-                                .edgesIgnoringSafeArea(.top)
-                                .frame(width: 140, height: 140)
-                                .offset(y: -180)// Pin the image to the top
+                Image("spl-signup") // Replace "signup-img" with your image name
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.65)
+                    .edgesIgnoringSafeArea(.top)
+                    .frame(width: 140, height: 140)
+                    .offset(y: -180)// Pin the image to the top
 
                 VStack {
                     // SignUpView content
@@ -27,7 +27,46 @@ struct SignUpView: View {
             Rectangle()
                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.6)
                 .foregroundColor(Color(red: 0.988, green: 0.87, blue: 0.82)) // Set the background color to match the image background
-                .offset(y: UIScreen.main.bounds.height * 0.35) // Adjust the offset to place the rectangle at the bottom
+                .offset(y: UIScreen.main.bounds.height * 0.41) // Adjust the offset to place the rectangle at the bottom
+                
+            VStack(spacing: 23) { // Adjust spacing between buttons
+                Button(action: {
+                    // Action for the first button
+                }) {
+                    Text("Sign up Free")
+                        .font(.headline) // Adjust font size
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 130) // Adjust horizontal padding to make the button broader
+                .padding(.vertical, 20) // Adjust vertical padding if needed
+                .background(Color(red: 0.37, green: 0.16, blue: 0.12))
+                .cornerRadius(30) // Adjust corner radius
+
+                Button(action: {
+                    // Action for the second button
+                }) {
+                    Text("Continue with Google")
+                        .font(.headline) // Adjust font size
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 95) // Adjust horizontal padding to make the button broader
+                .padding(.vertical, 20) // Adjust vertical padding if needed
+                .background(Color(red: 0.37, green: 0.16, blue: 0.12))
+                .cornerRadius(30) // Adjust corner radius
+
+                Button(action: {
+                    // Action for the third button
+                }) {
+                    Text("Continue with Facebook")
+                        .font(.headline) // Adjust font size
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 80) // Adjust horizontal padding to make the button broader
+                .padding(.vertical, 20) // Adjust vertical padding if needed
+                .background(Color(red: 0.37, green: 0.16, blue: 0.12))
+                .cornerRadius(30) // Adjust corner radius
+            }
+            .offset(y: UIScreen.main.bounds.height * 0.2)
         }
         .navigationBarHidden(true)
     }
@@ -38,3 +77,4 @@ struct SignUpView_Previews: PreviewProvider {
         SignUpView()
     }
 }
+
