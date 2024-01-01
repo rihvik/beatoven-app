49ef1133319a2273ed0715607baf5903de636a05
mkswagger <mathangykrishna16@gmail.com>
2024-01-01 23:47:29 +0530

    initial setup for home page

--- TabBarControllerBeatOven/ContentView.swift
+++ TabBarControllerBeatOven/ContentView.swift
@@ -17,6 +17,29 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             VStack {
+                // Add the text on top of the images
+                Text("Hi")
+                    .font(
+                        Font.custom("Catamaran", size: 18)
+                            .weight(.bold)
+                    )
+                    .foregroundColor(.black)
+                    .frame(width: 24.38644, height: 29.64382, alignment: .topLeading)
+
+                // Add the image at the top
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 62.75348, height: 66.36922)
+                    .background(
+                        Image("home-prof1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 62.753475189208984, height: 66.36921691894531)
+                            .clipped()
+                    )
+                    .cornerRadius(66.36922)
+                    .padding(.bottom, 10) // Add some padding between the image and TabView
+
                 TabView(selection: $selectedTab) {
                     ForEach(Tab.allCases, id: \.rawValue) { tab in
                         if tab.rawValue == "person" {
@@ -24,11 +47,19 @@ struct ContentView: View {
                                 ProfilePage()
                             }
                             .tag(tab)
+                            .tabItem {
+                                // Empty view to hide the default tab bar item
+                                EmptyView()
+                            }
                         } else if tab.rawValue == "person.2" {
                             HStack {
                                 DiscoverView()
                             }
-                            .tag(tab) // Corrected here, it should be 'tab' instead of 'tab.raw'
+                            .tag(tab)
+                            .tabItem {
+                                // Empty view to hide the default tab bar item
+                                EmptyView()
+                            }
                         } else {
                             HStack {
                                 Image(systemName: tab.rawValue)
@@ -40,7 +71,12 @@ struct ContentView: View {
                         }
                     }
                 }
+                .padding(.horizontal, 0)
+                .padding(.top, 0)
+                .padding(.bottom, 14)
+                .background(Color("home-color"))
             }
+
             VStack {
                 CustomTabBar(selectedTab: $selectedTab)
             }
@@ -54,4 +90,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
